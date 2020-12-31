8bbb71296e63d024f30433f9cf29416f25bf9374
Joe Marlo <joe.marlo.09@gmail.com>
2020-12-31 11:46:04 -0500

    added clustering and cleaned up repo

--- Plots/ggplot_settings.R
+++ Plots/ggplot_settings.R
@@ -1,56 +1,47 @@
-library(viridis)
+require(viridis)
 
 # build custom theme
-theme_custom <- function() {
-  theme_gray() +
-    theme(
-      panel.grid.minor.y = element_line(color = NA),
-      panel.grid.major.y = element_line(color = "gray95"),
-      panel.grid.minor.x = element_line(color = NA),
-      panel.grid.major.x = element_line(color = "gray95"),
-      panel.background = element_rect(fill = NA),
-      # plot.background = element_rect(
-      #   fill = NA,
-      #   color = "gray95",
-      #   size = 10
-      # ),
-      plot.margin = unit(c(0.5, 0.5, 0.5, 0.5), "cm"),
-      axis.title = element_text(color = "gray30"),
-      axis.ticks = element_line(color = NA),
-      strip.background = element_rect(fill = "gray95"),
-      strip.text = element_text(
-        color = "gray30",
-        size = 11,
-        face = "bold"
-      ),
-      plot.title = element_text(color = "gray30",
-                                face = "bold"),
-      plot.subtitle = element_text(size = 10,
-                                   color = "gray30"),
-      text = element_text(family = "Helvetica"),
-      plot.caption = element_text(face = "italic",
-                                  size = 6,
-                                  color = 'grey50'),
-      legend.title = element_blank(),
-      legend.position = 'bottom',
-      legend.key = element_rect(fill = NA)
-    )
-}
+theme_custom <- function()
+  theme_minimal() +
+  theme(
+    strip.background = element_rect(
+      fill = "gray95",
+      color = 'white'),
+    strip.text = element_text(
+      color = "gray30",
+      size = 11,
+      face = "bold"
+    ),
+    text = element_text(family = "Helvetica",
+                        color = "gray30"),
+    axis.title = element_text(
+      hjust = 0.99),
+      # size = 12),
+    axis.title.x = element_text(vjust = -1),
+    legend.direction = "horizontal", 
+    legend.position = "bottom",
+    legend.justification = "right",
+    plot.caption = element_text(face = "italic",
+                                size = 8,
+                                color = 'grey50')
+  )
 
+# set custom theme
 theme_set(theme_custom())
 
-# set default continuous colors
-options(
-  ggplot2.continuous.colour = "viridis",
-  ggplot2.continuous.fill = "viridis"
-)
-
-save_plot <- function(filename, device = 'png', height = 4, width = 6, ...){
-  # wrapper to ggsave with defaults
-  ggsave(...,
-         filename = paste0(filename, ".", device),
-         device = device,
-         height = height,
-         width = width
-         )
-}
+# # set default continuous colors
+# options(
+#   ggplot2.continuous.colour = "viridis",
+#   ggplot2.continuous.fill = "viridis"
+# )
+# 
+# # set default discrete colors
+# scale_colour_discrete <- function(...) {
+#   scale_color_viridis(..., discrete = TRUE)
+# }
+# scale_color_discrete <- function(...) {
+#   scale_color_viridis(..., discrete = TRUE)
+# }
+# scale_fill_discrete <- function(...) {
+#   scale_fill_viridis(..., discrete = TRUE)
+# }
--- README.md
+++ README.md
@@ -1,32 +1,4 @@
-# Modeling loneliness on sub-populations of Americans using sequence techniques and multi-level models
+# Who spends their day similar to you?
 
-Finding differences in time spent alone among subpopulations of Americans. First, by clustering respondents' activities by applying [sequence analysis techniques](https://en.wikipedia.org/wiki/Social_sequence_analysis) à la [Abbot](https://www.jstor.org/stable/2780695) — string-editing techniques, dissimilarity matrices, and hierarchical clustering. Then using cluster membership and year as nested variables in multi-level models to predict time spent alone.
 
 See also: [ATUS repo](https://github.com/joemarlo/ATUS)
-
-
-<br>
-<br>
-<p align="center">
-<img src="Plots/mean_alone_time.png" width=79%>
-</p>
-
-<br>
-<p align="center">
-<img src="Analyses/Levenshtein/Plots/Levenshtein_dendrogram.png" width=79%>
-</p>
-
-<br>
-<p align="center">
-<img src="Analyses/Levenshtein/Plots/Levenshtein_silhouette.png" width=79%>
-</p>
-
-<br>
-<p align="center">
-<img src="Analyses/Levenshtein/Plots/Levenshtein_proportion_plots.png" width=79%>
-</p>
-
-<br>
-<p align="center">
-<img src="Plots/negative_binomial_estimates.png" width=79%>
-</p>
